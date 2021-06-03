f2f063f183f9439012a7f7e3da5e056d352f6ff5
saravana <saravanakumarvs23@gmail.com>
2021-06-03 14:17:14 +0530

    Added few basic lines

--- Testcase1.py
+++ Testcase1.py
@@ -1,2 +1,20 @@
+#Example:1
+
 print("Welcome")
 print("Hello world!!!!")
+
+print( " ===================== ")
+#Example:2
+
+print("Hello world")
+print("What is your name : ")
+myname = input()
+print("Nice to meet you," + myname)
+print("Length of your name is:" )
+print(len(myname))
+print("What is your Age : ")
+Age = input()
+print("Your Age is : " + Age + " & You will be " + str(int(Age)+1) + " in a year.")
+
+
+
